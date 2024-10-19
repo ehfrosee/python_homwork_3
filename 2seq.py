127212147015b44078966797174e45f4d215d641
ehfrosee <34818798+ehfrosee@users.noreply.github.com>
2024-10-19 19:07:19 +0300

    Update 2seq.py
    Внесена корректировка

--- 2seq.py
+++ 2seq.py
@@ -1,8 +1,13 @@
 digit_string = input('Введите цифры. (допустимый разделитель: , ; /)')
 seps = [',', ';', '/']
 
+unique_list = []
 for sep in seps:
   if digit_string.find(sep) != -1:
-    dig_list = list(set(digit_string.split(sep)))
+    dig_list = digit_string.split(sep)
+    dig_set = set(digit_string.split(sep))
+    for dig in dig_set:
+      if dig_list.count(dig) == 1:
+        unique_list.append(dig)
     break
-print(dig_list)
+print(unique_list)
